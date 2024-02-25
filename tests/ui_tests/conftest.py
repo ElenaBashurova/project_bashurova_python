18b92aaa43f7ca8af13318cee85a043badca1f62
Elena Bashurova <110354016+ElenaBashurova@users.noreply.github.com>
2024-02-25 23:25:50 +0300

    UI_project

--- tests/ui_tests/conftest.py
+++ tests/ui_tests/conftest.py
@@ -1,36 +1,51 @@
+import os
 import pytest
-from selene import browser
-from utils import attach
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selene import browser
+from utils import attach
+from dotenv import load_dotenv
 
 
-@pytest.fixture(scope='function')
-def browser_configs(request):
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.base_url = 'https://www.officemag.ru'
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_configs():
     options = Options()
+
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+
     selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
+        'browserName': 'chrome',
+        'browserVersion': '100.0',
+        'selenoid:options': {'enableVNC': True, 'enableVideo': True},
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
+        command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
+        options=options,
     )
 
-    browser.config.driver = driver
+    # browser.config.driver = driver
+    browser.config.base_url = 'https://www.officemag.ru'
+    browser.config.timeout = 10
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield
 
-    attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
+    attach.add_html(browser)
     attach.add_video(browser)
+
     browser.quit()
