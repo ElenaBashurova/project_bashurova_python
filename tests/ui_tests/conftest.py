7982ef1dccd14b7ff2e7d2dbed47569d117bb055
Elena Bashurova <110354016+ElenaBashurova@users.noreply.github.com>
2024-02-26 21:33:56 +0300

    UI_project

--- tests/ui_tests/conftest.py
+++ tests/ui_tests/conftest.py
@@ -15,28 +15,27 @@ def load_env():
 @pytest.fixture(scope='function', autouse=True)
 def browser_configs():
     options = Options()
-
-
-    selenoid_capabilities = {
-        'browserName': 'chrome',
-        'browserVersion': '100.0',
-        'selenoid:options': {'enableVNC': True, 'enableVideo': True},
-    }
-    options.capabilities.update(selenoid_capabilities)
-
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-
-    driver = webdriver.Remote(
-        command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
-        options=options,
-    )
-
-    browser.config.driver = driver
     browser.config.base_url = 'https://www.officemag.ru'
     browser.config.timeout = 10
     browser.config.window_width = 1920
     browser.config.window_height = 1080
+
+    # selenoid_capabilities = {
+    #     'browserName': 'chrome',
+    #     'browserVersion': '100.0',
+    #     'selenoid:options': {'enableVNC': True, 'enableVideo': True},
+    # }
+    # options.capabilities.update(selenoid_capabilities)
+    #
+    # login = os.getenv('LOGIN')
+    # password = os.getenv('PASSWORD')
+    #
+    # driver = webdriver.Remote(
+    #     command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
+    #     options=options,
+    # )
+    #
+    # browser.config.driver = driver
 
     yield
 
