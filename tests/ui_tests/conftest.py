0e88b979481a4ae07cd61eb101ea40142ea269f6
Elena Bashurova <110354016+ElenaBashurova@users.noreply.github.com>
2024-02-26 21:52:05 +0300

    UI_project

--- tests/ui_tests/conftest.py
+++ tests/ui_tests/conftest.py
@@ -1,47 +1,47 @@
-import os
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selene import browser
+from selenium import webdriver
+from configs import config, credentials
 from utils import attach
-from dotenv import load_dotenv
 
 
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    load_dotenv()
-
-
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture()
 def browser_configs():
-    options = Options()
-    browser.config.base_url = 'https://www.officemag.ru'
-    browser.config.timeout = 10
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+    browser.config.base_url = config.base_url
+    browser.config.timeout = config.timeout
+    browser.config.window_height = config.window_height
+    browser.config.window_width = config.window_width
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = config.load_strategy
+    browser.config.driver_options = driver_options
 
-    # selenoid_capabilities = {
-    #     'browserName': 'chrome',
-    #     'browserVersion': '100.0',
-    #     'selenoid:options': {'enableVNC': True, 'enableVideo': True},
-    # }
-    # options.capabilities.update(selenoid_capabilities)
-    #
-    # login = os.getenv('LOGIN')
-    # password = os.getenv('PASSWORD')
-    #
-    # driver = webdriver.Remote(
-    #     command_executor=f'https://{login}:{password}@selenoid.autotests.cloud/wd/hub',
-    #     options=options,
-    # )
-    #
-    # browser.config.driver = driver
+    if config.environment == 'remote':
+        selenoid_capabilities = {
+            "browserName": config.driver_name,
+            "browserVersion": config.remote_version,
+            "selenoid:options": {
+                "enableVNC": config.remote_enableVNC,
+                "enableVideo": config.remote_enableVideo
+            }
+        }
+
+        driver_options.capabilities.update(selenoid_capabilities)
+
+        driver = webdriver.Remote(
+            command_executor=credentials(),
+            options=driver_options
+        )
+
+        browser.config.driver = driver
+
+    else:
+        browser.config.driver_name = config.driver_name
 
     yield
 
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # attach.add_html(browser)
-    # attach.add_video(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
